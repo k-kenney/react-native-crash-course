8c502f1f9db9e0bd1e30e6ecc90f48e3ef2ba8cd
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-05-21 20:31:20 -0600

    created user session code to create an account

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,25 +1,40 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import { createUser } from "../../lib/appwrite";
 
 const SignUp = () => {
   const [form, setForm] = useState({
-    username: '',
-    email: '',
-    password: ''
+    username: "",
+    email: "",
+    password: "",
   });
 
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = () => {
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert("Error", "Please fill in all of the fields");
+    }
+    setIsSubmitting(true);
+
+    try {
+      const result = await createUser(form.email, form.password, form.username)
+
+      // set it to global state..
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
     createUser();
-  }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full ">
@@ -54,20 +69,23 @@ const SignUp = () => {
             value={form.password}
             handleChangeText={(e) => setForm({ ...form, password: e })}
           />
-          <CustomButton 
-          title="Sign-up"
-          handlePress={submit}
-          containerStyles="mt-7"
-          isLoading={isSubmitting}
+          <CustomButton
+            title="Sign-up"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
           />
 
           <View className="justify-center pt-5 flex-row gap-2">
             <Text className="text-lg text-gray-100 font-pregular">
               Have an account already?
             </Text>
-            <Link href="/sign-in"
-            className="text-lg font-psemibold text-secondary">
-            Sign In</Link>
+            <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign In
+            </Link>
           </View>
         </View>
       </ScrollView>
